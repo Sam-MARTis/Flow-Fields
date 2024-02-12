7dfc970457504388e8f56fbf638812459cdd6783
Sam-MARTis <samanthmartis@gmail.com>
2024-02-13 00:49:02 +0530

    feat: added acceleration and touch control

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ let width;
 let height;
 let timeOld;
 let effect;
+let delTime = 0;
+let clickState = 0;
+let mouseX = 0;
+let mouseY = 0;
 //Onload function
 window.onload = () => {
   canvas = document.getElementById("canvas");
@@ -28,8 +32,12 @@ class Particle {
     this.radius = radius;
     this.color = color;
     this.velocity = {
-      x: Math.random() * 30 - 15,
-      y: Math.random() * 30 - 15,
+      x: Math.random() * 10 - 5,
+      y: Math.random() * 10 - 5,
+    };
+    this.acceleration = {
+      x: 0,
+      y: 0,
     };
   }
 
@@ -76,30 +84,59 @@ class Effect {
     }
   }
   updateDots() {
+    // let timeNew = Date.now();
+    delTime = parseInt(Date.now() - timeOld);
+    // console.log(delTime);
     this.#context.clearRect(0, 0, this.width, this.height);
 
-    console.log("Animating");
-    this.particles.forEach((particle) => {
+    // console.log("Animating");
+    this.particles.forEach((particle, delTime) => {
       let x = particle.x;
       let y = particle.y;
-      x += particle.velocity.x * 0.2;
-      y += particle.velocity.y * 0.2;
-      if (x >= this.width - particle.radius) {
-        x = this.width - particle.radius;
+      let r = particle.radius;
+      // console.log(delTime);
+      x += particle.velocity.x * 0.002 * delTime;
+      y += particle.velocity.y * 0.002 * delTime;
+
+      if (x >= this.width - r) {
+        x = this.width - r;
         particle.velocity.x *= -1;
-      } else if (x <= particle.radius) {
-        x = particle.radius;
+      } else if (x <= r) {
+        x = r;
         particle.velocity.x *= -1;
-      } else if (y >= this.height - particle.radius) {
-        y = this.height - particle.radius;
+      } else if (y >= this.height - r) {
+        y = this.height - r;
         particle.velocity.y *= -1;
-      } else if (y <= particle.radius) {
-        y = particle.radius;
+      } else if (y <= r) {
+        y = r;
         particle.velocity.y *= -1;
       }
       particle.x = x;
       particle.y = y;
+      //Acceleration
+      if (clickState) {
+        let distanceMouse = Math.sqrt(
+          (mouseX - particle.x) ** 2 + (mouseY - particle.y) ** 2
+        );
+        particle.acceleration.x =
+          (50 * ((particle.x - mouseX) / Math.abs(mouseX - particle.x))) /
+          (100*(distanceMouse/100)**2);
+        particle.acceleration.y =
+          (50 * ((particle.y - mouseY) / Math.abs(mouseY - particle.y))) /
+          (100 * (distanceMouse / 100) ** 4);
+        if (particle.acceleration.x > 2) particle.acceleration.x = 2;
+        if (particle.acceleration.y > 2) particle.acceleration.y = 2;
+        if (particle.acceleration.x < -2) particle.acceleration.x = -2;
+        if (particle.acceleration.y < -2) particle.acceleration.y = -2;
+        particle.velocity.x += particle.acceleration.x;
+        particle.velocity.y += particle.acceleration.y;
+      } else {
+        particle.acceleration.x = 0;
+        particle.acceleration.y = 0;
+      }
+
       particle.draw();
+      timeOld = Date.now();
       this.particles.forEach((particle2) => {
         let x2 = particle2.x;
         let y2 = particle2.y;
@@ -107,11 +144,11 @@ class Effect {
         if (d < 200) {
           this.#context.save();
           this.#context.beginPath();
-          this.#context.globalAlpha = 1 - d / 200;
+          this.#context.globalAlpha = (1 - d / 200) ** 1.2;
           this.#context.moveTo(x, y);
-          this.#context.strokeStyle= "white";
+          this.#context.strokeStyle = `hsl(${Math.random() * 30}, 100%, 50%)`;
           this.#context.lineTo(x2, y2);
-          this.#context.stroke()
+          this.#context.stroke();
           this.#context.restore();
         }
       });
@@ -122,14 +159,26 @@ class Effect {
 //Main function
 const main = () => {
   effect = new Effect(context, window.innerWidth, window.innerHeight, 10);
-  effect.addDots(100);
+  effect.addDots(200);
   setInterval(() => {
     effect.updateDots();
-  }, 20);
+  }, 10);
 };
 
 addEventListener("resize", () => {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
   effect.changeDims(canvas.width, canvas.height);
+});
+// addEventListener("keydown", (e) => { console.log(e) })
+addEventListener("mousedown", (e) => {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+  clickState = 1;
+});
+addEventListener("mouseup", (e) => {
+  
+  
+  clickState = 0;
+  console.log(e);
 });
